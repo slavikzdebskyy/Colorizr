a9f839f70224b96bcfe66b8f379077a5d9d49a3d
slavikzdebskyy <zdebskyy.slavik@gmail.com>
2018-12-11 16:59:05 +0200

    initial project

---
+++ src/components/App.component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class AppComponent extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Create APP !!!</h1>
+      </div>
+    );
+  }
+}
+
+export default AppComponent;
---
+++ src/components/color.picker.componenet.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class AppComponent extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default AppComponent;
