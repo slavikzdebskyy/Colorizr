42adb657db179aba239bd33c55ee2b271dedad38
slavikzdebskyy <zdebskyy.slavik@gmail.com>
2018-12-14 18:38:41 +0200

    Updates

---
+++ precache-manifest.3a92c42e56f3bd86807b8cf682c2fad1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a246dfdff448cfd31478674b9faae79",
+    "url": "https://slavikzdebskyy.github.io/colorizr.github.io/static/media/logo-light.8a246dfd.svg"
+  },
+  {
+    "revision": "fbc0676afc4650d5f2707185e75eff7f",
+    "url": "https://slavikzdebskyy.github.io/colorizr.github.io/static/media/logo-dark.fbc0676a.svg"
+  },
+  {
+    "revision": "9928f17a8061057b0022",
+    "url": "https://slavikzdebskyy.github.io/colorizr.github.io/static/js/runtime~main.9928f17a.js"
+  },
+  {
+    "revision": "86d67bc99017058170ba",
+    "url": "https://slavikzdebskyy.github.io/colorizr.github.io/static/js/main.86d67bc9.chunk.js"
+  },
+  {
+    "revision": "369411a6dc9e382fda56",
+    "url": "https://slavikzdebskyy.github.io/colorizr.github.io/static/js/1.369411a6.chunk.js"
+  },
+  {
+    "revision": "86d67bc99017058170ba",
+    "url": "https://slavikzdebskyy.github.io/colorizr.github.io/static/css/main.e1bdb318.chunk.css"
+  },
+  {
+    "revision": "369411a6dc9e382fda56",
+    "url": "https://slavikzdebskyy.github.io/colorizr.github.io/static/css/1.6cc2a1f4.chunk.css"
+  },
+  {
+    "revision": "83916122402fe9e55586183290a9ce50",
+    "url": "https://slavikzdebskyy.github.io/colorizr.github.io/index.html"
+  }
+];
