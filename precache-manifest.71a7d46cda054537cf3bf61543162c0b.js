0b4c09dca617daed46c79643c1e92ccbf959196c
slavikzdebskyy <zdebskyy.slavik@gmail.com>
2018-12-14 16:27:46 +0200

    Updates

---
+++ precache-manifest.71a7d46cda054537cf3bf61543162c0b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a246dfdff448cfd31478674b9faae79",
+    "url": "/colorizr.github.io/static/media/logo-light.8a246dfd.svg"
+  },
+  {
+    "revision": "fbc0676afc4650d5f2707185e75eff7f",
+    "url": "/colorizr.github.io/static/media/logo-dark.fbc0676a.svg"
+  },
+  {
+    "revision": "8ea55d2a9e7775e2ef5b",
+    "url": "/colorizr.github.io/static/js/runtime~main.8ea55d2a.js"
+  },
+  {
+    "revision": "225395897b6ad551c736",
+    "url": "/colorizr.github.io/static/js/main.22539589.chunk.js"
+  },
+  {
+    "revision": "369411a6dc9e382fda56",
+    "url": "/colorizr.github.io/static/js/1.369411a6.chunk.js"
+  },
+  {
+    "revision": "225395897b6ad551c736",
+    "url": "/colorizr.github.io/static/css/main.e1bdb318.chunk.css"
+  },
+  {
+    "revision": "369411a6dc9e382fda56",
+    "url": "/colorizr.github.io/static/css/1.6cc2a1f4.chunk.css"
+  },
+  {
+    "revision": "646c06c999bafa1f3805faac307f7c85",
+    "url": "/colorizr.github.io/index.html"
+  }
+];
