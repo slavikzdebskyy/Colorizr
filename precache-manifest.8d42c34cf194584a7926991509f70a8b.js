edc64ba7ae8a29cfe50e071c6d0691f37241bb0d
slavikzdebskyy <zdebskyy.slavik@gmail.com>
2018-12-14 16:51:31 +0200

    Updates

---
+++ precache-manifest.8d42c34cf194584a7926991509f70a8b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a246dfdff448cfd31478674b9faae79",
+    "url": "/static/media/logo-light.8a246dfd.svg"
+  },
+  {
+    "revision": "fbc0676afc4650d5f2707185e75eff7f",
+    "url": "/static/media/logo-dark.fbc0676a.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9e5f0f4efd74fd17ca74",
+    "url": "/static/js/main.9e5f0f4e.chunk.js"
+  },
+  {
+    "revision": "369411a6dc9e382fda56",
+    "url": "/static/js/1.369411a6.chunk.js"
+  },
+  {
+    "revision": "9e5f0f4efd74fd17ca74",
+    "url": "/static/css/main.e1bdb318.chunk.css"
+  },
+  {
+    "revision": "369411a6dc9e382fda56",
+    "url": "/static/css/1.6cc2a1f4.chunk.css"
+  },
+  {
+    "revision": "c51426e2a73b1a864730d217561a8984",
+    "url": "/index.html"
+  }
+];
